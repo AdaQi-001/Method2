0db4211591a73488feb6230bc25c7f8806e2827c
CharlotQi <97003885+CharlotQi@users.noreply.github.com>
2022-05-02 00:01:11 -0700

    edition2

--- testMethodz.java
+++ testMethodz.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
-public class testMethod {
+public class testMethodz {
     @Test
     public void testMinusOne(){
         assertEquals(1, methodz.minusOne(1)); //expect to be failed
